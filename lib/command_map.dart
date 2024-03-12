f2d0b4b2ef09b47feaee050f026eddeb74dbe4c9
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-12 13:14:27 +0700

    try catch block to avoid the app crashing

--- lib/command_map.dart
+++ lib/command_map.dart
@@ -1,6 +1,14 @@
+import "package:dart_pokeapi/api/pokeapi.dart";
+
 import "./api/location_area_req.dart";
 
 Future<void> callbackMap() async {
-  final locationArea = await getLocationAreas(null);
-  print(locationArea.toString());
+  final res = await getLocationAreas(Client.nextURL);
+  print("Location areas:");
+  for (var result in res!.results) {
+    print("-${result.name}");
+  }
+
+  Client.nextURL = res.next;
+  Client.previousURL = res.previous;
 }
