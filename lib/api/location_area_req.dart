5f9d569b750edbbb68ff08ae7f84fe1b44408cf6
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-12 13:48:58 +0700

    handle error on bad request and null resp

--- lib/api/location_area_req.dart
+++ lib/api/location_area_req.dart
@@ -3,31 +3,26 @@ import "package:dart_pokeapi/api/pokeapi.dart";
 import "./types_api_declare.dart";
 
 Future<LocationAreaResp?> getLocationAreas(String? pageURL) async {
-  try {
-    const endpoint = "/location-area";
-    String fullURL = Client.baseURL + endpoint;
+  const endpoint = "/location-area/23213ewqdsadas";
+  String fullURL = Client.baseURL + endpoint;
 
-    if (pageURL != null) {
-      fullURL = pageURL;
-    }
-
-    final connectClient = Client.httpClient;
-    final req = await connectClient.getUrl(Uri.parse(fullURL));
-    final res = await req.close();
-
-    if (res.statusCode > 299) {
-      throw Exception("Bad status code: ${res.statusCode}");
-    }
-
-    final resBody = await res.transform(utf8.decoder).join();
-
-    if (resBody.isEmpty) {
-      throw Exception("Receive an empty response");
-    }
-
-    return LocationAreaResp.fromJson(json.decode(resBody));
-  } catch (err) {
-    print("Error: $err");
-    return null;
+  if (pageURL != null) {
+    fullURL = pageURL;
   }
+
+  final connectClient = Client.httpClient;
+  final req = await connectClient.getUrl(Uri.parse(fullURL));
+  final res = await req.close();
+
+  if (res.statusCode > 299) {
+    throw Exception("Bad status code: ${res.statusCode}");
+  }
+
+  final resBody = await res.transform(utf8.decoder).join();
+
+  if (resBody.isEmpty) {
+    throw Exception("Receive an empty response");
+  }
+
+  return LocationAreaResp.fromJson(json.decode(resBody));
 }
--- lib/command_map.dart
+++ lib/command_map.dart
@@ -3,17 +3,25 @@ import "package:dart_pokeapi/api/pokeapi.dart";
 import "./api/location_area_req.dart";
 
 Future<void> callbackMap() async {
-  final res = await getLocationAreas(Client.nextURL);
-  print("Location areas:");
+  try {
+    final res = await getLocationAreas(Client.nextURL);
 
-  for (var result in res!.results) {
-    print("-${result.name}");
+    if (res == null) {
+      throw Exception("Failed to get location areas");
+    }
+    print("Location areas:");
+
+    for (var result in res.results) {
+      print("-${result.name}");
+    }
+
+    print("");
+
+    Client.nextURL = res.next;
+    Client.previousURL = res.previous;
+  } catch (err) {
+    print(err);
   }
-
-  print("");
-
-  Client.nextURL = res.next;
-  Client.previousURL = res.previous;
 }
 
 Future<void> callbackMapb() async {
