4f0733d6970af080a39c8b6c589c65c9b4257412
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-12 13:13:55 +0700

    implement next and previous url to client connect

--- lib/api/location_area_req.dart
+++ lib/api/location_area_req.dart
@@ -1,29 +1,33 @@
 import "dart:convert";
-
 import "package:dart_pokeapi/api/pokeapi.dart";
-
 import "./types_api_declare.dart";
 
 Future<LocationAreaResp?> getLocationAreas(String? pageURL) async {
-  const endpoint = "/location-area";
-  String fullURL = Client.baseURL + endpoint;
+  try {
+    const endpoint = "/location-area";
+    String fullURL = Client.baseURL + endpoint;
 
-  if (pageURL != null) {
-    fullURL = pageURL;
-  }
+    if (pageURL != null) {
+      fullURL = pageURL;
+    }
 
-  final connectClient = Client.httpClient;
-  final req = await connectClient.getUrl(Uri.parse(fullURL));
-  final res = await req.close();
+    final connectClient = Client.httpClient;
+    final req = await connectClient.getUrl(Uri.parse(fullURL));
+    final res = await req.close();
 
-  if (res.statusCode > 399) {
-    print("Bad error code: ${res.statusCode}");
+    if (res.statusCode > 299) {
+      throw Exception("Bad status code: ${res.statusCode}");
+    }
+
+    final resBody = await res.transform(utf8.decoder).join();
+
+    if (resBody.isEmpty) {
+      throw Exception("Receive an empty response");
+    }
+
+    return LocationAreaResp.fromJson(json.decode(resBody));
+  } catch (err) {
+    print("Error: $err");
     return null;
   }
-
-  final resBody = await res.transform(utf8.decoder).join();
-  LocationAreaResp locationArea =
-      LocationAreaResp.fromJson(json.decode(resBody));
-
-  return locationArea;
 }
