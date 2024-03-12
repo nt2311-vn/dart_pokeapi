af477c052a40a0d4852661b7029f98a19d6203ec
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-12 11:53:42 +0700

    client request will have next and previous url

--- lib/api/pokeapi.dart
+++ lib/api/pokeapi.dart
@@ -3,4 +3,6 @@ import 'dart:io';
 class Client {
   static const String baseURL = "https://pokeapi.co/api/v2/";
   static final HttpClient httpClient = HttpClient();
+  static final String? nextURL = null;
+  static final String? previousURL = null;
 }
